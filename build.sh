f66a78547de05176fd3f8289c003067de2b0cbda
name <rocacruzaxel48@gmail.com>
2024-06-22 20:10:08 -0500

    page

---
+++ build.sh
@@ -0,0 +1,5 @@
+# build.sh
+sed -i 's/{\% if page.edit_url \%}/<div style="float:right"><span id="busuanzi_container_page_pv"><font size="2" color="grey">本文总阅读量<span id="busuanzi_value_page_pv"><\/span>次<\/font><\/span><\/div>{\% if page.edit_url \%}/g' mkdocs-material/material/base.html
+
+mkdocs build
+mkdocs serve
---
+++ docs/index.md
@@ -0,0 +1,17 @@
+---
+comments: true
+hide:
+  - navigation
+---
+# Home
+Apuntes
+## About 
+
+![profile](imgs/profile.jpg){ align=left width="200" }
+
+Contact:
+
+- :material-mail: (Email) ： `axel.rocca.c@uni.pe`
+- :simple-github: Github ：[Qzxell (github.com)](https://github.com/Qzxell)
+  
+SAD
